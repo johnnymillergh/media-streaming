8297f99461cb65d82107e3a292cce7daad466c24
Johnny Miller (锺俊) <johnnysviva@outlook.com>
2020-10-16 17:47:11 +0800

    fix: delete unknown tag `date`
    Error:  Exit code: 1 -
    /home/runner/work/media-streaming/media-streaming/media-streaming-spring-boot-autoconfigure/src/main/java/com/jmframework/boot/mediastreamingspringbootautoconfigure/Placeholder.java:7:
    error: unknown tag: date
    Error:   * @date 10/16/2020 5:41 PM
    Error:     ^

--- media-streaming-sample-app/src/main/java/com/jmsoftware/boot/mediastreamingsampleapp/MediaStreamingSampleAppApplication.java
+++ media-streaming-sample-app/src/main/java/com/jmsoftware/boot/mediastreamingsampleapp/MediaStreamingSampleAppApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * Description: MediaStreamingSampleAppApplication, change description here.
  *
  * @author Johnny Miller (鍾俊), email: johnnysviva@outlook.com
- * @date 10/16/2020 2:58 PM
+ * date 10/16/2020 2:58 PM
  **/
 @SpringBootApplication
 public class MediaStreamingSampleAppApplication {
