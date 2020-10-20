1a367a6adc873626a8f1d2a0fc4391f635c575b4
Johnny Miller (锺俊) <johnnysviva@outlook.com>
2020-10-20 14:21:28 +0800

    test(sample-app): correct unit test for API

--- media-streaming-sample-app/src/test/java/com/jmsoftware/boot/mediastreamingsampleapp/MediaStreamingSampleAppApplicationTests.java
+++ media-streaming-sample-app/src/test/java/com/jmsoftware/boot/mediastreamingsampleapp/MediaStreamingSampleAppApplicationTests.java
@@ -1,10 +1,16 @@
 package com.jmsoftware.boot.mediastreamingsampleapp;
 
+import com.jmframework.boot.mediastreamingspringbootautoconfigure.model.Video;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.EntityExchangeResult;
 import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.util.List;
 
 /**
  * Description: MediaStreamingSampleAppApplicationTests, change description here.
@@ -12,13 +18,23 @@ import org.springframework.test.web.reactive.server.WebTestClient;
  * @author Johnny Miller (锺俊), email: johnnysviva@outlook.com, date: 10/20/2020 1:04 PM
  **/
 @Slf4j
-@WebFluxTest
+@SpringBootTest
+@AutoConfigureWebTestClient
 class MediaStreamingSampleAppApplicationTests {
     @Autowired
     private WebTestClient webTestClient;
 
     @Test
     void videosTest() {
-        webTestClient.get().uri("/videos").exchange().expectStatus().isOk();
+        EntityExchangeResult<List<Video>> returnResult = webTestClient
+                .get()
+                .uri("/videos")
+                .accept(MediaType.ALL)
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .expectBodyList(Video.class)
+                .returnResult();
+        log.info("Video test: {}", returnResult.getResponseBody());
     }
 }
