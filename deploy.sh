7c6c734bc593b505eb30e02157eeb59179a3836b
Johnny Miller (锺俊) <johnnysviva@outlook.com>
2020-10-22 14:05:14 +0800

    build(deploy): enhance deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -11,22 +11,28 @@ set -e
 
 # Check the variables are set
 if [ -z "$OSSRH_USERNAME" ]; then
-  echo "missing environment value: OSSRH_USERNAME" >&2
+  echo "[Deployment ERROR] Missing environment value: OSSRH_USERNAME" >&2
   exit 1
 fi
 
 if [ -z "$OSSRH_PASSWORD" ]; then
-  echo "missing environment value: OSSRH_PASSWORD" >&2
+  echo "[Deployment ERROR] Missing environment value: OSSRH_PASSWORD" >&2
   exit 1
 fi
 
 if [ -z "$GPG_KEY_NAME" ]; then
-  echo "missing environment value: GPG_KEY_NAME" >&2
+  echo "[Deployment ERROR] Missing environment value: GPG_KEY_NAME" >&2
   exit 1
 fi
 
 if [ -z "$GPG_PASSPHRASE" ]; then
-  echo "missing environment value: GPG_PASSPHRASE" >&2
+  echo "[Deployment ERROR] Missing environment value: GPG_PASSPHRASE" >&2
+  exit 1
+fi
+
+# If file .travis/gpg.asc not exists
+if [ ! -f .travis/gpg.asc ]; then
+  echo "[Deployment ERROR] Missing environment value: GPG_PASSPHRASE" >&2
   exit 1
 fi
 
@@ -34,11 +40,12 @@ fi
 # beforehand)
 gpg --fast-import .travis/gpg.asc
 
-if [ ! -z "$TRAVIS_TAG" ]; then
-  echo "on a tag -> set pom.xml <version> to $TRAVIS_TAG"
+# If `TRAVIS_TAG` string is not empty
+if [ -n "$TRAVIS_TAG" ]; then
+  echo "On a tag -> set pom.xml <version> to $TRAVIS_TAG"
   mvn --settings "${TRAVIS_BUILD_DIR}/.travis/mvn-settings.xml" org.codehaus.mojo:versions-maven-plugin:2.7:set -DnewVersion=$TRAVIS_TAG 1>/dev/null 2>/dev/null
 else
-  echo "not on a tag -> keep snapshot version in pom.xml"
+  echo "Not on a tag -> keep snapshot version in pom.xml"
 fi
 
 # Run the maven deploy steps
