5750e06e4624146c112ce6ebc2e983d2c1b8220a
Johnny Miller (锺俊) <johnnysviva@outlook.com>
2020-10-26 20:22:12 +0800

    perf(Travis): beautify bash command

--- .travis/install.sh
+++ .travis/install.sh
@@ -4,13 +4,10 @@
 # https://stackoverflow.com/questions/19622198/what-does-set-e-mean-in-a-bash-script
 set -e
 
-function now() {
-  echo date +%Y-%m-%d' '%H:%M:%S.%N | cut -b 1-23
-  return 0
-}
+NOW=$(date +%Y-%m-%d' '%H:%M:%S.%N | cut -b 1-23)
 
 CURRENT_DIR=$(pwd)
-echo "$(now)  INFO --- [INSTALL] CURRENT_DIR: $CURRENT_DIR"
+echo "$NOW  INFO --- [INSTALL] CURRENT_DIR: $CURRENT_DIR"
 command "ls"
 
 # Run the Maven clean install
@@ -19,8 +16,8 @@ command "ls"
 INSTALL_COMMAND_RESULT=$?
 
 if [ "$INSTALL_COMMAND_RESULT" -eq 0 ]; then
-  echo "$(now)  INFO --- [INSTALL] Installation succeed. INSTALL_COMMAND_RESULT: $INSTALL_COMMAND_RESULT"
+  echo "$NOW  INFO --- [INSTALL] Installation succeed. INSTALL_COMMAND_RESULT: $INSTALL_COMMAND_RESULT"
 else
-  echo "$(now)  INFO --- [INSTALL] Installation failed. INSTALL_COMMAND_RESULT: $INSTALL_COMMAND_RESULT" >&2
+  echo "$NOW ERROR --- [INSTALL] Installation failed. INSTALL_COMMAND_RESULT: $INSTALL_COMMAND_RESULT" >&2
   exit 1
 fi
