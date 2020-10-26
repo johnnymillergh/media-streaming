b83035a24767410bd316068f073f3d19a06f969e
Johnny Miller (锺俊) <johnnysviva@outlook.com>
2020-10-26 11:20:01 +0800

    test(Travis): update install.sh

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+CURRENT_DIR=$(pwd)
+
+echo "[INSTALL] INFO CURRENT_DIR: $CURRENT_DIR"
+
 # Run the maven install
 ./mvnw clean install -Dmaven.javadoc.skip=true -Dgpg.skip=true --quiet --batch-mode --show-version
