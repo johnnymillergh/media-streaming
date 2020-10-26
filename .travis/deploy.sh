c7e2c2a3756b6aa4801cf3fad09dd1a873c7f2b4
Johnny Miller (锺俊) <johnnysviva@outlook.com>
2020-10-26 21:25:37 +0800

    refactor(Bash): abstract now() function

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -11,24 +11,28 @@
 # https://stackoverflow.com/questions/19622198/what-does-set-e-mean-in-a-bash-script
 set -e
 
-NOW=$(date +%Y-%m-%d' '%H:%M:%S.%N | cut -b 1-23)
-
+########################## Functions Declaration ##########################
 # Bash tips: Colors and formatting (ANSI/VT100 Control sequences)
 # https://misc.flogisoft.com/bash/tip_colors_and_formatting
 # Pass arguments into a function
 # https://bash.cyberciti.biz/guide/Pass_arguments_into_a_function
+function now() {
+  nowVariable=$(date +%Y-%m-%d' '%H:%M:%S.%N | cut -b 1-23)
+  echo "$nowVariable"
+}
+
 function printInfo() {
-  echo -e "$NOW \e[32m INFO --- $1\e[0m"
+  echo -e "$(now) \e[32m INFO --- $1\e[0m"
   return 0
 }
 
 function printWarn() {
-  echo -e "$NOW \e[33m WARN --- $1\e[0m"
+  echo -e "$(now) \e[33m WARN --- $1\e[0m"
   return 0
 }
 
 function printError() {
-  echo -e "$NOW \e[31mERROR --- $1\e[0m"
+  echo -e "$(now) \e[31mERROR --- $1\e[0m"
   return 0
 }
 
